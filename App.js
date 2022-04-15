bc9d043845f64180abee6dfb6f11db3a32cd448a
JeeFzPiires <jeferson.piires@gmail.com>
2022-04-15 17:46:11 -0300

    Consumindo API

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import api from './src/services/Api';
+
+
 
 export default function App() {
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get("/users/jeefzpiires")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+            <Text style={styles.title}>Consumindo Api</Text>
+            <StatusBar style="auto" />
+
+            <Text style={styles.titleUser}>Usuario: {user?.login}</Text>
+            <Text style={styles.titleBio}>Bio: {user?.bio}</Text>
+
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#343434',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 36,
+    color: '#c9b00c',
+    marginTop: 200,
+    borderRadius: 10,
+    margin: 10,
+
+  },
+  titleUser: {
+    fontSize: 24,
+    color: '#c9b00c',
+    margin: 100,
+    borderColor: 'rgba(255, 255, 255, 0.1)',
+    borderWidth: 2,
+  },
+  titleBio: {
+    fontSize: 24,
+    color: '#c9b55c',
+    padding: 20,
   },
 });
